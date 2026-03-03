5ad93a518a67a4d99ceaf38e2d5d415e8a68f1b9
mysterymartial <bolasax16@gmail.com>
2026-03-03 14:11:45 +0100

    Add rewrite for /cv.pdf to serve CV

--- myPortfolioWeb/frontend/next.config.js
+++ myPortfolioWeb/frontend/next.config.js
@@ -23,6 +23,7 @@ const nextConfig = {
     return [
       { source: '/AGBAOSI%20BOLARINWA%20MINASU%20MYSTERY%20CV.PDF', destination: '/AGBAOSI%20BOLARINWA%20MINASU%20MYSTERY%20CV.pdf' },
       { source: '/AGBAOSI BOLARINWA MINASU MYSTERY CV.PDF', destination: '/AGBAOSI%20BOLARINWA%20MINASU%20MYSTERY%20CV.pdf' },
+      { source: '/cv.pdf', destination: '/AGBAOSI%20BOLARINWA%20MINASU%20MYSTERY%20CV.pdf' },
     ];
   },
   async headers() {
